50421496a00204486cb75a91226919ede4868201
Harsh <bhatiaharsh000@gmail.com>
2026-02-20 22:59:49 +0400

    Added Commit bean

---
+++ src/main/java/org/example/assignment1/model/Comment.java
@@ -0,0 +1,45 @@
+import org.springframework.stereotype.Component;
+
+@Component
+public class Comment {
+
+    private String id;
+    private String eventid;
+    private String studentid;
+    private String content;
+
+    public Comment() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEventid() {
+        return eventid;
+    }
+
+    public void setEventid(String eventid) {
+        this.eventid = eventid;
+    }
+
+    public String getStudentid() {
+        return studentid;
+    }
+
+    public void setStudentid(String studentid) {
+        this.studentid = studentid;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
