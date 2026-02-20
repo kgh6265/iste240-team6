a7e371ed055b2c3e67bd56bc18daf1ff910690d9
Aditya Avinash <136869159+Aditya-Avinash07@users.noreply.github.com>
2026-02-20 22:26:20 +0400

    Added Event bean to the file

---
+++ src/main/java/org/example/assignment1/model/Event.java
@@ -0,0 +1,56 @@
+package org.example.assignment1.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Event {
+
+    private String id;
+    private String title;
+    private String description;
+    private String category;
+    private String status;
+
+    public Event() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
